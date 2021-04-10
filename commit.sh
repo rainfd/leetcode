18c9436b1fe6a938acdc7bd98e555ae78c86d240
RainFD <rainfd@live.cn>
2021-04-10 18:04:15 +0800

    new leet script

--- commit.sh
+++ commit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# add commit
+set -ex
+
 pros=$(git diff README.md | grep "+|" | awk 'NR==1{printf("%s", $2)} NR>1{ printf(", %s", $2) }')
 test -n "${pros}" && commit="Add ${pros} solutions."
 echo "${commit}"
